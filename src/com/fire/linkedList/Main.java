1520c670092f434ffae6a37997ab02694efd1c5a
jiefeng <jiefengfire@163.com>
2019-11-06 10:08:30 +0800

    添加了忽略信息

--- src/com/fire/linkedList/Main.java
+++ src/com/fire/linkedList/Main.java
@@ -1,9 +1,24 @@
 package com.fire.linkedList;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,1511,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-        System.out.println(Sum.sum(arr));
+//        int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,1511,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+//        System.out.println(Sum.sum(arr));
+//        new Main().a();
+        String qq = "5114@qq.com";
+        System.out.println(qq.contains("@qq.com"));
+        Map map = new HashMap();
+        map.put(1,1);
+        map.put(1,1);
+        System.out.println(map.get(2));
+    }
+
+    public void a(){
+        System.out.println("a");
+        a();
     }
 }
