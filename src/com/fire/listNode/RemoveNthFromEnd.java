1cc0861b00ab7157cc69a128efc7c7e6044cabfa
jiefeng <jiefengfire@163.com>
2019-12-13 10:03:27 +0800

    移除倒数第n个节点，第二个思路

--- src/com/fire/listNode/RemoveNthFromEnd.java
+++ src/com/fire/listNode/RemoveNthFromEnd.java
@@ -4,7 +4,7 @@
  * @Author: jiefeng
  * @Date: 2019-12-13 09:45:17
  * @LastEditors: jiefeng
- * @LastEditTime: 2019-12-13 09:52:36
+ * @LastEditTime: 2019-12-13 10:02:52
  */
 package com.fire.listNode;
 
@@ -14,6 +14,11 @@ package com.fire.listNode;
  * 获取长度
  * 倒数第n个，就是l-n+1个
  * 将其替换
+ * 第二个思路：双指针
+ * 首先第一个指针遍历到n+1个位置
+ * 之后第一个第二个指针一起遍历，当第一个指针到头的时候停止
+ * 此时第二个指针就是倒数第n个
+ * 替换即可
  */
 public class RemoveNthFromEnd {
 
@@ -26,7 +31,9 @@ public class RemoveNthFromEnd {
             length++;
             first = first.next;
         }
+        // 获取倒数第n个的位置
         length -= n;
+        // 从哑节点开始遍历
         first = dummy;
         while (length > 0) {
             length--;
