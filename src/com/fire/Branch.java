a795254a97a75344b658adc0ab6dbd175c4b2589
jiefeng <jiefengfire@163.com>
2020-01-09 16:04:35 +0800

    new_branch file

---
+++ src/com/fire/Branch.java
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: 
+ * @Author: jiefeng.w@foxmail.com
+ * @Date: 2020-01-09 16:04:01
+ * @LastEditors: jiefeng
+ * @LastEditTime: 2020-01-09 16:04:01
+ */
+package com.fire;
+
+/**
+ * Branch
+ */
+public class Branch {
+
+    
+}
