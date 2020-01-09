981b09d0162c058ca3efa6f9415506713d62d49a
jiefeng <jiefengfire@163.com>
2020-01-09 16:09:58 +0800

    new

--- src/com/fire/Branch.java
+++ src/com/fire/Branch.java
@@ -2,8 +2,8 @@
  * @Descripttion: 
  * @Author: jiefeng.w@foxmail.com
  * @Date: 2020-01-09 16:04:01
- * @LastEditors: jiefeng
- * @LastEditTime: 2020-01-09 16:04:01
+ * @LastEditors  : jiefeng
+ * @LastEditTime : 2020-01-09 16:09:44
  */
 package com.fire;
 
@@ -12,5 +12,7 @@ package com.fire;
  */
 public class Branch {
 
-    
+    public static void main(String[] args) {
+        
+    }
 }
